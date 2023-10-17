02c2326892680e2f1601e1f4f2b87cbd11649ce9
Liam Bolan <AQ231688@aquinas.ac.uk>
2023-10-17 13:47:52 +0100

    Add project files.

---
+++ 17.10.23/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._10._23_quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader questions = new StreamReader("questions.txt");
+            string username;
+            int score = 0;
+            while (!questions.EndOfStream)
+            {
+                string qstions = questions.ReadLine();
+                
+                Console.WriteLine(qstions);
+               
+                
+                
+                string a1 = questions.ReadLine();   
+                Console.WriteLine($"A){a1}");
+                string a2 = questions.ReadLine();
+                Console.WriteLine($"B){a2}");
+                string a3 = questions.ReadLine();
+                Console.WriteLine($"C){a3}");
+                string a4 = questions.ReadLine();
+                Console.WriteLine($"D){a4}");
+                string correct = questions.ReadLine();
+                string useranswer = " ";
+                
+                while (!"ABCD".Contains(useranswer[0]))
+                {
+                    Console.Write("Enter the answer: A) B) C) D)");
+                    useranswer = Console.ReadLine().ToUpper();
+                    
+                }
+                if (useranswer == correct)
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
+                
+                
+            }
+            Console.WriteLine($"You scored: {score}");
+            Console.WriteLine("Please Enter your name");
+            username = questions.ReadLine();
+
+        }
+    }
+}
